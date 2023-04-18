ef84a8c734eead8944c2a685084cf78527f0a219
Andreas Schneider <as@MARS-SOLUTIONS.loc>
2023-04-18 11:21:39 +0200

    some exercises done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from './basicHighlight/basicHighlight.directive';
+import { BetterHighlightDirective } from './betterHighlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
